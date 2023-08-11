75b352130c41fd69436a390ffd1c71503da7b9b2
Ariel Oliveira <ariel96viana@gmail.com>
2023-08-11 18:08:12 -0300

    Criadas duas funções, uma de multiplicação e outra de saudação, recebendo valores e dando retorno de acordo com eles

---
+++ src/funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaNumeros(numero1, numero2) {
+    return numero1 * numero2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicaNumeros(10, 20));
+console.log(saudacao("Ariel"));
---
+++ src/funcoes.ts
@@ -0,0 +1,10 @@
+function multiplicaNumeros(numero1: number, numero2: number) {
+  return numero1 * numero2;
+}
+
+function saudacao(nome: string) {
+  return `Olá ${nome}`;
+}
+
+console.log(multiplicaNumeros(10, 20));
+console.log(saudacao("Ariel"));
